fa8c3a1cf70f540315f94ec83af6bf8c176ada7f
Sullivan Joro <119728329+Sullivan1301@users.noreply.github.com>
2025-06-21 09:34:28 +0300

    feat(design): integrate brand colors and typography into Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,22 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}", 
+    "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#171c5e',
+        'antique-gold': '#b28c1c',
+        'golden-beige': '#e5d49e',
+        'off-white': '#f0f2f5',
+      },
+      fontFamily: {
+        'title': ['"Montserrat Alternates"', 'sans-serif'],
+        'body': ['"Open Sans"', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
