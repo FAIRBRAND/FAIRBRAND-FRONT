7893e7a9e3f647d999a139300f80cd7cd905db0e
mvjdev <hei.maria.625@gmail.com>
2025-06-22 22:41:59 +0300

    chore: layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,8 +1,25 @@
 import "./globals.css";
+//import { Montserrat_Alternates, Open_Sans } from "next/font/google";
 import Footer from "@/app/(components)/Footer";
 import Providers from "./providers";
 import Header from "../(components)/Header";
 
+/*
+const montserratAlternates = Montserrat_Alternates({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-title",
+  weight: ["400", "700"],
+});
+
+const openSans = Open_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-body",
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+});
+*/
 
 export default async function RootLayout({
   children,
