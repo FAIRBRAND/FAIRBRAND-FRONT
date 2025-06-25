3ef58f1c5829ee28202a37ea7606f35a5e673aed
mvjdev <hei.maria.625@gmail.com>
2025-06-25 07:50:07 +0300

    fix: responsivity

--- src/app/(components)/success/GlobalSuccessSection.tsx
+++ src/app/(components)/success/GlobalSuccessSection.tsx
@@ -5,15 +5,15 @@ import BenefitsCard from "./BenefitsCard";
 
 export default function GlobalSuccessSection() {
   return (
-    <section className="py-16 px-4 pt-20 pb-20 pl-2 pr-20  bg-golden-beige">
+    <section className="p-6 pt-20 pb-20 bg-golden-beige">
       <div className="container mx-auto max-w-7xl">
         <div className="grid lg:grid-cols-2 gap-12 lg:gap-16 items-center">
-          {/* Left - CTA Content */}
+          {/* Left */}
           <div>
             <UnlockPathSection />
           </div>
 
-          {/* Right - Benefits Card */}
+          {/* Right */}
           <div>
             <BenefitsCard />
           </div>
