71935f2857d62bb257b419f163b9961edc327911
mvjdev <hei.maria.625@gmail.com>
2025-06-25 06:39:14 +0300

    fix: conflict

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,12 @@
-<<<<<<< HEAD
-// app/layout.tsx
-export default function RootBaseLayout({
-=======
 import "./globals.css";
 
 export default function RootLayout({
->>>>>>> f281148 (Revert "Merge pull request #10 from FAIRBRAND/feature/header")
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-<<<<<<< HEAD
-    <html>
-=======
     <html lang="en">
->>>>>>> f281148 (Revert "Merge pull request #10 from FAIRBRAND/feature/header")
       <body>{children}</body>
     </html>
   );
