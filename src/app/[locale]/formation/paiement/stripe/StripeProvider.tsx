cc8fecd4a8059cb5977112aef6e0ef3a89509692
judilegend <judilegend2@gmail.com>
2025-10-04 00:47:17 +0300

    feat: add StripeProvider component for Stripe integration

---
+++ src/app/[locale]/formation/paiement/stripe/StripeProvider.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!);
+
+export default function StripeProviderComponent({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <Elements stripe={stripePromise}>{children}</Elements>;
+}
